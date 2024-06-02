0bd5f6871b14165229927ff5be58b5d82b6e2020
Caio Marcelo <caio.marcelo79@gmail.com>
2024-06-02 00:05:21 -0300

    Arquivos iniciais front-end e back-end

---
+++ back-end/src/main/java/com/softmoda/softmoda/SoftmodaApplication.java
@@ -0,0 +1,13 @@
+package com.softmoda.softmoda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftmodaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftmodaApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/softmoda/softmoda/SoftmodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softmoda.softmoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftmodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
